2e71807fcffd6a4107c7fd9a45b432b5b0995250
Felipe <feipeTM@gmail.com>
2019-01-15 17:42:16 +0100

    Update Create-Acme-Handy-Worker.sql

--- War y Scripts/Create-Acme-Handy-Worker.sql
+++ War y Scripts/Create-Acme-Handy-Worker.sql
@@ -1,3 +1,21 @@
+﻿start transaction;
+
+create database 'Acme-Handy-Worker';
+
+use 'Acme-Handy-Worker';
+
+
+create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete 
+	on `Acme-Handy-Worker`.* to 'acme-user'@'%';
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine,     
+	alter routine, execute, trigger, show view
+	 on `Acme-Handy-Worker`.* to 'acme-manager'@'%';
+
+
 -- MySQL dump 10.13  Distrib 5.5.29, for Win64 (x86)
 --
 -- Host: localhost    Database: Acme-Handy-Worker
@@ -1321,3 +1339,5 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-01-15 17:28:37
+
+commit;
